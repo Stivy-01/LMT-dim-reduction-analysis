faf16e002f8c1c3a6a9fe184a76c9a9d2502c95f
Stivy-01 <andreastivala.as@gmail.com>
2025-01-30 21:45:13 +0100

    Simplify setup.py to use pyproject.toml configuration

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="lmt_analysis",
-    version="0.1.0",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    description="LMT Dimensionality Reduction Analysis Toolkit",
-    author="Andrea Stivala",
-    author_email="andreastivala.as@gmail.com",
-    install_requires=[
-        'numpy==1.23.5',
-        'scipy>=1.9.0',
-        'pandas',
-        'scikit-learn>=1.0.0',
-        'plotly>=5.13.0',
-        'tkcalendar>=1.6.1',
-        'seaborn>=0.12.2',
-        'pathlib',
-    ],
-    python_requires='>=3.8',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-    ],
-) 
+setup() 
